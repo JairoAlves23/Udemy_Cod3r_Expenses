10facc61fc5a1aa9f246684c1d3a2dff3050367d
Jairo Alves <jairocreta@hotmail.com>
2020-02-16 20:00:24 -0300

    Screenshot_1581893980_aula4

--- lib/home.dart
+++ lib/home.dart
@@ -43,12 +43,19 @@ class _HomeState extends State<Home> {
                     child: Row(
                   children: <Widget>[
                     Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      padding: EdgeInsets.all(10),    
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2)),
                       child: Text(tr.value.toString()),
-                    ), 
-                    Column(children: <Widget>[
-                      Text(tr.title),
-                      Text(tr.date.toString()),
-                    ],)
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    )
                   ],
                 ));
               }).toList(),
