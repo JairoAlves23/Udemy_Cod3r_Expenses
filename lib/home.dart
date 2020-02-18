f7b868a03af952f774b5e22750459d7eefa91358
Jairo Alves <jairocreta@hotmail.com>
2020-02-17 22:27:33 -0300

    Screenshot_1581989191_aula5

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,7 +12,7 @@ class _HomeState extends State<Home> {
     Transaction(
         id: 't1',
         title: 'Novo Tênis de corrida',
-        value: 310.76,
+        value: 315.76,
         date: DateTime.now()),
     Transaction(
         id: 't2', title: 'Conta de Luz', value: 211.30, date: DateTime.now()),
@@ -45,15 +46,25 @@ class _HomeState extends State<Home> {
                     Container(
                       margin:
                           EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                      padding: EdgeInsets.all(10),    
+                      padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black, width: 2)),
-                      child: Text(tr.value.toString()),
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                            fontSize: 20),
+                      ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tr.title),
-                        Text(tr.date.toString()),
+                        Text(
+                          tr.title,
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),
+                        ),
+                        Text(DateFormat('d/MM/y').format(tr.date), style: TextStyle(color: Colors.grey),),
                       ],
                     )
                   ],
